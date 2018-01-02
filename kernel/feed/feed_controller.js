f06b810f1b6203a8b5bceea6bacbd98c5ccb88e1
lalugeo <lalu.george@ifb-group.com>
2018-01-02 17:19:08 +0100

    skeleton added

---
+++ kernel/feed/feed_controller.js
@@ -0,0 +1,33 @@
+/**
+* Module that handles all the opertations related to managing feed configuration in the persistant storage
+* @module FeedKernel/FeedController
+*/
+
+var FeedController=(function(){
+  var _FeedController={};
+
+  var _CurrentFeeds=[];
+
+  _FeedController.InsertNewFeedForMonitoring=function(feed){
+
+  };
+
+  _FeedController.GetAllFeeds=function(feed){
+
+  };
+
+  _FeedController.GetAFeed=function(feedId){
+    return feed;
+  };
+
+
+  _FeedController.Init=function(){
+    Notifications.Show("InitController");
+  };
+
+  _FeedController.UpdateAFeed=function(feed){
+
+  };
+
+  return _FeedController;
+}());
