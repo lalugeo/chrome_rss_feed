6bdb3285ad7d087517a4e724eb3bd40b7e0b472f
lalugeo <lalu.george@ifb-group.com>
2018-01-05 14:06:17 +0100

    linted for #20

--- feed_notifier/kernel/kernel.js
+++ feed_notifier/kernel/kernel.js
@@ -1,6 +1,8 @@
+/* globals chrome,FeedController, FeedReader, Notifications */
 
 /**
-* All the operations related to the extension, like installation, update, initialisation etc is handled here
+* All the operations related to the extension, like installation, update,
+* initialisation etc is handled here
 * @module FeedKernel
 */
 
@@ -17,31 +19,28 @@
 //   });
 // });
 
-chrome.runtime.onStartup.addListener(function() {
+chrome.runtime.onStartup.addListener(() => {
   //  console.log("got startup");
   // Notifications.Show("StartupMessage");
-    // chrome.windows.create('../render/configure_feeder.html', {
-    //   id: "ConfigureFeeder",
-    //   innerBounds: {
-    //     width: 500,
-    //     height: 600,
-    //     minWidth: 500,
-    //     minHeight: 600
-    //   },
-    //   frame: 'none'
-    // });
-    console.log("got startup");
-    FeedController.Init()
-    .then(FeedReader.RegisterAllFeeds)
-    ;
-    Notifications.Show("WelcomeMessage");
+  // chrome.windows.create('../render/configure_feeder.html', {
+  //   id: "ConfigureFeeder",
+  //   innerBounds: {
+  //     width: 500,
+  //     height: 600,
+  //     minWidth: 500,
+  //     minHeight: 600
+  //   },
+  //   frame: 'none'
+  // });
+  // console.log("got startup");
+  FeedController.Init()
+    .then(FeedReader.RegisterAllFeeds);
+  Notifications.Show("WelcomeMessage");
 });
 
 
-chrome.runtime.onInstalled.addListener(function() {
-  console.log("Installed the runtime for feed notifier");
+chrome.runtime.onInstalled.addListener(() => {
   FeedController.Init()
-  .then(FeedReader.RegisterAllFeeds)
-  ;
+    .then(FeedReader.RegisterAllFeeds);
   Notifications.Show("WelcomeMessage");
 });
